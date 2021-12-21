e668d8118417a67f129447e1cb962dbc623c6a5b
datrandomboi <datrandomboi@users.noreply.github.com>
2021-12-20 20:10:49 -0500

    tiny changes

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,7 +7,7 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // [kcc] instead of putting the keycode directly we can make vars for it at the top of the script
+        
     }
 
     // Update is called once per frame
