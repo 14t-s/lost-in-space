bd04d4099f07cf3453341eed27da1f44ac702e9f
apadac <68026656+apadac@users.noreply.github.com>
2022-01-05 23:54:16 -0500

    Initial scene change class

---
+++ Assets/Scripts/SceneLoader/Loader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        TestScene1,
+
+    }
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+
+}
