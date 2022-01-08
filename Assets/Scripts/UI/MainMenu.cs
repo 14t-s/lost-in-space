679f78883d4542487fd57b641ddaf243995ce327
apadac <68026656+apadac@users.noreply.github.com>
2022-01-07 23:24:40 -0500

    Implemented main menu
    Still missing options and savegames

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TestScene1");
+    }
+    public void ContinueGame()
+    {
+        //Application.Quit();
+    }
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
